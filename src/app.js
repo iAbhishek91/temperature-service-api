9b6f1fcd7ebe6acf7883036acb9e750f049da208
Abhishek Das <abhishek.das1@lloydsbanking.com>
2019-03-23 10:32:52 +0000

    feat(body-parser): middleware for parsing body

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import documentRouter from './routers/v1/documentation';
 import logger from './middleware/logger';
 
 const app = express();
 
 app.use(logger);
+app.use(bodyParser.json());
 
 app.get('/', (_, req) => {
   req.status(200);
